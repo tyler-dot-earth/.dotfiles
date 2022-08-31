584a97cdb3b1dc87827d313d319e4502f14dba79
Tyler Nieman <tyler@tsnieman.net>
2022-08-31 16:45:04 -0700

    tweak lspsaga config

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -46,13 +46,129 @@ local saga = require 'lspsaga'
 -- saga.init_lsp_saga() -- use default config
 saga.init_lsp_saga({
 	-- rename_action_quit = "<C-c>",
+
+	-- "single" | "double" | "rounded" | "bold" | "plus"
 	border_style = 'rounded',
+
+	--the range of 0 for fully opaque window (disabled) to 100 for fully
+	--transparent background. Values between 0-30 are typically most useful.
+	saga_winblend = 20,
+
+	-- when cursor in saga window you config these to move
+	move_in_saga = {
+		prev = '<C-p>',
+		next = '<C-n>'
+	},
+
+	-- Error, Warn, Info, Hint
+	-- use emoji like
+	-- { "🙀", "😿", "😾", "😺" }
+	-- or
+	-- { "😡", "😥", "😤", "😐" }
+	-- and diagnostic_header can be a function type
+	-- must return a string and when diagnostic_header
+	-- is function type it will have a param `entry`
+	-- entry is a table type has these filed
+	-- { bufnr, code, col, end_col, end_lnum, lnum, message, severity, source }
+	diagnostic_header = { " ", " ", " ", "ﴞ " },
+
+	-- show diagnostic source
+	show_diagnostic_source = true,
+
+	-- add bracket or something with diagnostic source, just have 2 elements
+	diagnostic_source_bracket = {},
+
+	-- preview lines of lsp_finder and definition preview
+	max_preview_lines = 10,
+
+	-- use emoji lightbulb in default
+	code_action_icon = "💡",
+
+	-- if true can press number to execute the codeaction in codeaction window
+	code_action_num_shortcut = true,
+
+	-- same as nvim-lightbulb but async
+	code_action_lightbulb = {
+			enable = true,
+			enable_in_insert = true,
+			cache_code_action = true,
+			sign = true, -- show in "sign" column
+			update_time = 150,
+			sign_priority = 20,
+			virtual_text = false, -- show at "end" of line
+	},
+
+	-- finder icons
+	finder_icons = {
+		def = '  ',
+		ref = '諭 ',
+		link = '  ',
+	},
+
+	-- finder do lsp request timeout
+	-- if your project big enough or your server very slow
+	-- you may need to increase this value
+	finder_request_timeout = 1500,
+
+	-- set antoher colorscheme in preview window
+	finder_preview_hl_ns = 0,
+
+	finder_action_keys = {
+			open = "o",
+			vsplit = "s",
+			split = "i",
+			tabe = "t",
+			quit = "q",
+			scroll_down = "<C-f>",
+			scroll_up = "<C-b>", -- quit can be a table
+	},
+
 	code_action_keys = {
 		quit = "<ESC>", -- default: "q"
 		exec = "<CR>",
 	},
+
+	rename_action_quit = "<C-c>",
+
+	rename_in_select = true,
+
+	definition_preview_icon = "  ",
+
+	-- show symbols in winbar must nightly
+	symbol_in_winbar = {
+			in_custom = false,
+			enable = false,
+			separator = ' ',
+			show_file = true,
+			click_support = false,
+	},
+
+	-- show outline
+	show_outline = {
+		win_position = 'right',
+		--set special filetype win that outline window split.like NvimTree neotree
+		-- defx, db_ui
+		win_with = '',
+		win_width = 30,
+		auto_enter = true,
+		auto_preview = true,
+		virt_text = '┃',
+		jump_key = 'o',
+		-- auto refresh when change buffer
+		auto_refresh = true,
+	},
+
+	-- custom lsp kind
+	-- usage { Field = 'color code'} or {Field = {your icon, your color code}}
+	custom_kind = {},
+
+	-- if you don't use nvim-lspconfig you must pass your server name and
+	-- the related filetypes into this table
+	-- like server_filetype_map = { metals = { "sbt", "scala" } }
+	server_filetype_map = {},
 })
----- hover docs
+
+---- Hover docs
 vim.keymap.set(
 	"n",
 	"K",
@@ -62,7 +178,8 @@ vim.keymap.set(
 		desc = 'Hover docs',
 	}
 )
----- finder (see definition, references)
+
+---- Finder (see definition, references)
 vim.keymap.set(
 	"n",
 	"gh",
@@ -72,7 +189,8 @@ vim.keymap.set(
 		desc = 'Finder (definitions, refs)'
 	}
 )
----- Code action
+
+---- Code actions
 vim.keymap.set(
 	"n",
 	"<leader>ca",
@@ -101,6 +219,17 @@ vim.keymap.set(
 	{
 		silent = true,
 		desc = 'LSP: Preview definition'
+	}
+)
+
+-- Show line diagnostics
+vim.keymap.set(
+	"n",
+	"<leader>cd",
+	"<cmd>Lspsaga show_line_diagnostics<CR>",
+	{
+		silent = true,
+		desc = 'Show line diagnostics'
 	}
 )
 
