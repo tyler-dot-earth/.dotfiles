83351faa5234927dc9149710eb2bd06c2809fb25
Tyler Nieman <tyler@tsnieman.net>
2023-12-12 08:26:28 -0800

    add hydrate.nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1884,6 +1884,27 @@ require("lazy").setup({
 	{
 		"f-person/git-blame.nvim"
 	},
+
+	-- Hydration reminders
+	{
+		"stefanlogue/hydrate.nvim",
+		-- This installs the latest stable release.
+		-- Set to false or omit to install the latest development version
+		version = "*",
+		opts = {
+			-- The interval between notifications in minutes
+			minute_interval = 20,
+
+			-- The render style for notifications
+			-- Accepted values are "default", "minimal", "simple" or "compact"
+			render_style = "compact",
+
+			-- Loads time of last drink on startup
+			-- Useful if you don't have long-running neovim instances
+			-- or if you tend to have multiple instances running at a time
+			persist_timer = false
+		}
+	},
 }, {
 	-- 	https://github.com/folke/lazy.nvim#%EF%B8%8F-configuration
 	--
